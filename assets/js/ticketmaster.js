7c38e3e307a211abf82016e80a14ed6661e2bee5
mpj1693 <mpj1693@gmail.com>
2019-04-16 18:10:58 -0400

    deploy

--- assets/js/ticketmaster.js
+++ assets/js/ticketmaster.js
@@ -3,25 +3,22 @@
 
 uniqueArtistArray;
 
-
-
-
 // set state
 var stateCode = 'NY'
 
-
-//// METHOD B: to avoid "Too Many Requests" 429 error
 //// on click button trigger ajax request at a given interval looping through KeywordArray
 
 // timer id
 var timer;
 
 // On click event to retrive event data from TicketMaster API
-$("#buttons-area").on("click", function () {
-  // settimeout to run at 2 sec interval
-  timer = setTimeout(runAjax, 500);
-})
+$("#click-for-concerts").on("click", function () {
 
+  console.log(uniqueArtistArray + "Artist in TM")
+  
+  // settimeout to run at 2 sec interval
+  timer = setTimeout(runAjax, 1000);
+})
 
 // index counter for array
 var j = 0;
@@ -29,67 +26,15 @@ var j = 0;
 function runAjax() {
   $.ajax({
     type: "GET",
-    url: `https://app.ticketmaster.com/discovery/v2/events.json?apikey=GUyA0G4teAjiouvRwk3D2sDwgEHUAaa6&size=1&keyword=${uniqueArtistArray[j]}&stateCode=${stateCode}`,
+    url: `https://app.ticketmaster.com/discovery/v2/events.json?apikey=AEH5HGnHdiehb7Tei6mXUTXrULADEytJ&size=1&keyword=${uniqueArtistArray[j]}&stateCode=${stateCode}`,
     async: true,
     dataType: "json",
     success: function (json) {
       console.log(json);
 
-
-      //url working?
-      console.log("artists in tm " + uniqueArtistArray)
-      //console.log("URL: " + url);
-
-
-
-<<<<<<< HEAD
-      //loop through TMresponse array and write each concert on page
-      for (var i = 0; i < TMresponse.length; i++) {
-        var $name;
-        var $venue;
-        var $date;
-        var $time;
-        var $priceRangeMin;
-        var $priceRangeMax;
-        var $btnTM;
-
-        if (TMresponse[i].hasOwnProperty('priceRanges')) {
-          $priceRangeMin = TMresponse[i].priceRanges[0].min;
-          $priceRangeMax = TMresponse[i].priceRanges[0].max;
-        } else {
-          $priceRangeMin = "NO DATA";
-          $priceRangeMax = "NO DATA";
-        };
-
-        if (TMresponse[i].hasOwnProperty('name')) {
-          $name = TMresponse[i].name;
-        } else {
-          $name = "no name"
-        };
-
-        if (TMresponse[i].hasOwnProperty('_embedded')) {
-          $venue = TMresponse[i]._embedded.venues[0].name;
-        } else {
-          $venue = "Venue TBD"
-        };
-
-        if (TMresponse[i].hasOwnProperty('dates')) {
-          $date = TMresponse[i].dates.start.localDate;
-        } else {
-          $date = "Date TBD"
-        };
-
-        if (TMresponse[i].hasOwnProperty('dates')) {
-          $time = TMresponse[i].dates.start.localTime;
-        } else {
-          $time = "Time TBD"
-=======
       // checking if ticketmaster API has event information for the artist
       if (json.hasOwnProperty('_embedded')) {
         var TMresponse = json._embedded.events
-
-        console.log(TMresponse)
-
 
         //loop through TMresponse array and write each concert on page
         for (var i = 0; i < TMresponse.length; i++) {
@@ -139,6 +84,8 @@ function runAjax() {
             $btnTM = "No Event"
           }
 
+          
+
           console.log($name);
           console.log($venue);
           console.log($date);
@@ -155,20 +102,21 @@ function runAjax() {
             $("<td>").text($priceRangeMin + " - " + $priceRangeMax),
             $("<td>").append($btnTM),
           );
+          // newRow.addClass('table table-dark table-hover')
 
           $("tbody").append(newRow);
->>>>>>> 707a163c6cee3884bf165edf83b8a1b53bf201ee
         };
 
         // increase index counter
         j++;
-        console.log("this is counter:" + j);
+        console.log("this is j counter:" + j);
 
       // if event does not exist, skip to next artist in array
       } else {
         console.log("no event available for this artist");
         j++;
-        return runAjax();
+        
+        // return runAjax();
       }
       
     },
@@ -176,7 +124,7 @@ function runAjax() {
       if (j === uniqueArtistArray.length) {
         return clearTimeout(timer)
       }
-      timer = setTimeout(runAjax, 500);
+      timer = setTimeout(runAjax, 1000);
       console.log("next iteration")
     },
     error: function (xhr, status, err) {
