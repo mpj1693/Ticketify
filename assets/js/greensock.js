9f6ec2452976cdd97e5096000f17a76061a6347d
Isabella Torres <izzy612@gmail.com>
2019-04-16 15:38:57 -0400

    spotify button

---
+++ assets/js/greensock.js
@@ -0,0 +1,6 @@
+var header = $(".header");
+
+    lilHeader = $(".lil-header");
+
+        TweenLite.from(header, 1, {x: -200, ease:Power2.easeOut });
+TweenLite.from(lilHeader, 1, { autoAlpha: 0, delay: 1 });
